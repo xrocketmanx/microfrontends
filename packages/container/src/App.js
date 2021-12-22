c6355d2e367a3226a41ac49ca8eaa7ca28b76684
Oleksandr_Amirov <xrocketmanx007@gmail.com>
2021-12-22 11:46:13 +0200

    add auth logic

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,24 +1,32 @@
-import React from 'react';
+import React, { lazy, Suspense, useState } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { StylesProvider, createGenerateClassName } from '@material-ui/core';
 import Header from './components/Header';
-import MarketingApp from './components/MarketingApp';
-import AuthApp from './components/AuthApp';
+import Progress from './components/Progress';
+
+const MarketingLazy = lazy(() => import('./components/MarketingApp'));
+const AuthLazy = lazy(() => import('./components/AuthApp'));
 
 const generateClassName = createGenerateClassName({
   productionPrefix: 'co'
 });
 
 export default () => {
+  const [isSignedIn, setIsSignedIn] = useState(false);
+
   return (
     <StylesProvider generateClassName={generateClassName}>
       <BrowserRouter>
         <div>
-          <Header />
-          <Switch>
-            <Route path="/auth" component={AuthApp} />
-            <Route path="/" component={MarketingApp} />
-          </Switch>
+          <Header signedIn={isSignedIn} onSignOut={() => setIsSignedIn(false)} />
+          <Suspense fallback={<Progress />}>
+            <Switch>
+              <Route path="/auth">
+                <AuthLazy onSignIn={() => setIsSignedIn(true)} />
+              </Route>
+              <Route path="/" component={MarketingLazy} />
+            </Switch>
+          </Suspense>
         </div>
       </BrowserRouter>
     </StylesProvider>
--- packages/container/src/components/AuthApp.js
+++ packages/container/src/components/AuthApp.js
@@ -2,7 +2,7 @@ import React, { useEffect, useRef } from 'react';
 import { mount } from 'auth/AuthApp';
 import { useHistory } from 'react-router-dom';
 
-export default () => {
+export default ({ onSignIn }) => {
   const ref = useRef(null);
   const history = useHistory();
 
@@ -13,7 +13,8 @@ export default () => {
         if (history.location.pathname !== nextPathname) {
           history.push(nextPathname);
         }
-      }
+      },
+      onSignIn
     });
 
     history.listen(onParentNavigate);
